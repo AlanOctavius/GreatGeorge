f319dea98c029c6d5b7af025c789a8d318462217
AlanOctavius <obrienad@googlemail.com>
2014-04-15 16:08:00 +0100

    Created touching hostiles
    Created the touch hostile prefab and a hopefully general spawner

---
+++ Assets/Scripts/HostileSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HostileSpawner : MonoBehaviour {
+
+	public GameObject hostile;
+	public float spawnTime = 5f;		// The amount of time between each spawn.
+	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+
+	// Use this for initialization
+	void Start () {
+		// Start calling the Spawn function repeatedly after a delay .
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+
+	}
+	
+	void Spawn ()
+	{
+		// Instantiate a hostile.
+
+		Instantiate(hostile, transform.position, transform.rotation);
+		
+	
+	}
+}
---
+++ Assets/Scripts/TouchEnemy.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchEnemy : Character {
+
+
+	// Checks
+	public bool aheadContact = false;
+	public bool aheadGround = false;
+	public bool grounded = false;			// Whether or not the player is grounded.
+	private Transform aheadContactCheck;
+	private Transform aheadGroundCheck;
+	private Transform groundCheck;			// A position marking where to check if the player is grounded.
+
+	public bool debugAheadFloor = false;
+
+	//Movement
+	public float moveForce = 50f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 1f;				// The fastest the player can travel in the x axis.
+
+	public int movingDirection = 1;	// Bool to show direction
+	public bool changingDirection = false; //bool to show if direction changed last frame ie hit a wall
+
+
+	//Useful transform
+	private Vector3 invertYVec = new Vector3(-1,1,1);
+
+	// Use this for initialization
+	void Start () {
+		groundCheck = transform.FindChild ("Ground Check");
+		aheadContactCheck = transform.FindChild ("Ahead Check");
+		aheadGroundCheck = transform.FindChild ("Ahead Ground Check");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (changingDirection) {
+			aheadGroundCheck.localPosition =  Vector3.Scale( invertYVec , aheadGroundCheck.localPosition );
+			aheadContactCheck.localPosition =  Vector3.Scale( invertYVec , aheadContactCheck.localPosition );
+			movingDirection *= -1;
+			changingDirection = false;
+		}
+	
+		int mask = 1 << LayerMask.NameToLayer("Ground");
+		// if something is in the way or there is no floor set the change direction flag
+		aheadContact = Physics2D.Linecast (transform.position, aheadContactCheck.position,mask);
+		grounded = Physics2D.Linecast (transform.position, groundCheck.position,mask);
+		// Ahead ground fails if there is nothing ahead
+		aheadGround = !Physics2D.Linecast (transform.position, aheadGroundCheck.position,mask);
+
+		if (aheadGround) {
+			debugAheadFloor = true;
+				}
+
+		if ((aheadGround || aheadContact)&&grounded) {
+			changingDirection = true;
+				}
+
+		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+		if(Mathf.Abs(movingDirection * rigidbody2D.velocity.x )< maxSpeed && grounded) {
+			// ... add a force to the player.
+			rigidbody2D.AddForce(Vector2.right * movingDirection * moveForce);
+		}
+
+
+	}
+
+
+	void OnCollisionEnter2D(Collision2D collision){
+
+		if (collision.gameObject.tag == "Player") {
+			PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
+			player.TakeDamage(1);
+			Debug.Log("playerhit");
+			changingDirection = true;
+			}
+		if (collision.gameObject.tag == "Hostile"){
+			changingDirection = true;
+			}
+		}
+
+	protected override void Die(){
+		Destroy(gameObject);
+		}
+
+}
