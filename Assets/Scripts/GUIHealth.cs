dea9c8d6c058b0a990fa4e7b73c4db0c152a9537
AlanOctavius <obrienad@googlemail.com>
2014-04-26 00:44:18 +0100

    Health notification and score update
    Found a nicer gui method.

---
+++ Assets/Scripts/GUIHealth.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIHealth : MonoBehaviour {
+		
+	private PlayerHealth player;
+
+	void Start(){
+		player = GameObject.Find ("Player").GetComponent<PlayerHealth> ();
+
+		}
+
+	void OnGUI(){
+		GUI.Label (new Rect (0,0,100,50), "Health: " + player.Health);
+		}
+		
+	}
---
+++ Assets/Scripts/GUIScore.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIScore : MonoBehaviour {
+
+
+	void OnGUI(){
+		GUI.Label (new Rect (0,20,100,50), "ScoreGUI : " + GameManagerScript.GameScore.ToString ());
+	}
+	
+}
